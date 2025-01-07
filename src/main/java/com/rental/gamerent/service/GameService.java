5a0cab4380a20154d4c000590e67e133af1060b0
Amin Bihamta <97396978+AminBihamta@users.noreply.github.com>
2025-01-08 00:19:31 +0800

    Made each game clickable

--- src/main/java/com/rental/gamerent/service/GameService.java
+++ src/main/java/com/rental/gamerent/service/GameService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class GameService {
@@ -20,5 +21,10 @@ public class GameService {
             System.out.println("No games found");
         }
         return games;
+    }
+
+    public Game getGameById(Long id) {
+        Optional<Game> game = gameRepo.findById(id);
+        return game.orElse(null);
     }
 }
